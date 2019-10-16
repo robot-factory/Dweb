023dc05dfea55248f06366f6288a9c9c2d5c1e46
liuboheng <lbh9311@sina.com>
2019-10-17 00:14:58 +0800

    11

---
+++ p2p/p2p-project/scripts/nat_pmp_test.js
@@ -0,0 +1,26 @@
+var natpmp = require('nat-pmp');
+
+// create a "client" instance connecting to your local gateway
+var client = natpmp.connect('192.168.1.1');
+
+
+// explicitly ask for the current external IP address
+client.externalIp(function (err, info) {
+  if (err) throw err;
+  console.log(info);
+  console.log('Current external IP address: %s', info.ip.join('.'));
+});
+
+
+// setup a new port mapping
+client.portMapping({ private: 2222, public: 5555, ttl: 3600 }, function (err, info) {
+  if (err) throw err;
+  console.log(info);
+  // {
+  //   type: 'tcp',
+  //   epoch: 8922109,
+  //   private: 22,
+  //   public: 2222,
+  //   ...
+  // }
+});
